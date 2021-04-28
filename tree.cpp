7ebe83946263e10cb07bfa4c7d07128ab5b7b410
JJ <jjjmase123@gmail.com>
2021-04-27 22:05:20 -0700

    Added to tree.cpp

--- tree.cpp
+++ tree.cpp
@@ -12,6 +12,18 @@ boost::qvm::vec<float, 3> OctreeNode::get_max_bounds() {
   return bounding_box.get_max();
 }
 
+Particle *get_particle(size_t index) {
+  return particles[index];
+}
+
+size_t get_particle_count() {
+  return particles.size();
+}
+
+OctreeNode* get_child(size_t n) {
+  return children[n];
+}
+
 // Recursive function to generate oct tree called using
 void make_tree(OctreeNode &root, BoundingBox &box, Particles &particles) {
   // 
