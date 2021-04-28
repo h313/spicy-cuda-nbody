f8832f8779d6d5fda0c644375d3e11b9e6dfb8c6
JJ <jjjmase123@gmail.com>
2021-04-28 16:47:45 -0700

    In theory finished data output

--- data.cpp
+++ data.cpp
@@ -11,21 +11,6 @@ DataOutput::DataOutput(char* filename) {
 void write_bounding_box(BoundingBox *bb) {
     boost::qvm::vec<float, 3> min = bb->get_min();
     boost::qvm::vec<float, 3> end = bb->get_max();
-
-    // Edge A
-    // Edge B
-    // Edge C
-    // Edge D
-    // Edge E
-    // Edge F
-    // Edge G
-    // Edge H
-    // Edge I
-    // Edge J
-    // Edge K
-    // Edge L
-    
-
     output_stream << "BOUNDINGBOX " << A<0>(start) << " " << A<1>(start) << " " << A<2>(start) << " "
         A<0>(end) << " " << A<1>(end) << " " << A<2>(end) << std::endl:
 }
@@ -43,4 +28,11 @@ void add_datapoints(Particles *particles) {
 void add_octree_bounding_boxes(OctreeNode *parent) {
     // Write the current bounding box
     write_bounding_box(parent->get_bounding_box());
+
+    // Recurse down through children
+    for(int i = 0; i < 8; i++) {
+        OctreeNode *child = parent->get_child(i);
+        if(child->get_particle_count() > 1)
+            add_octree_bounding_boxes(child);
+    }
 }
