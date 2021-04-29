df0a75b5bf5c81035f95ff7a22681e1656b0c1c0
Haoda Wang <harry@h313.info>
2021-04-28 19:38:05 -0700

    Set number of particles

--- main.cpp
+++ main.cpp
@@ -1,17 +1,21 @@
 #include "data.h"
 #include "gravity_calculations.h"
 
-#include <boost/program_options.hpp>
 #include <iostream>
 
-using namespace boost::program_options;
+using namespace std;
 
 int main(int argc, char **argv) {
+  if (argc < 2) {
+    cout << "Usage: nbody <NUM_PARTICLES>" << endl;
+    return 1;
+  }
+
   BoundingBox bb(0, 0, 0, 100, 100, 100);
-  Particles particles = generate_random_particles(100, 0, 100);
+  Particles particles = generate_random_particles(atoi(argv[1]), 0, 100);
   OctreeNode *parent = new OctreeNode(particles, bb);
 
-  std::cout << "Completed particle generation!" << std::endl;
+  cout << "Completed particle generation!" << endl;
 
   // make_tree(static_cast<void *>(parent));
   make_tree_openmp(parent);
