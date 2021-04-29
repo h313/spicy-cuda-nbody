b573e4024e4ecf6664b6b9a23c8cd87ce853cc64
JJ <jjjmase123@gmail.com>
2021-04-28 19:30:32 -0700

    testing

--- data.cpp
+++ data.cpp
@@ -35,8 +35,8 @@ void DataOutput::add_octree_bounding_boxes(OctreeNode &parent) {
 
     // Recurse down through children
     for(int i = 0; i < 8; i++) {
-        OctreeNode &child = parent.get_child(i);
-        if(child->get_particle_count() > 1)
-            add_octree_bounding_boxes(child);
+        OctreeNode child = *parent.get_child(i);
+        if(child.get_particle_count() > 1)
+            add_octree_bounding_boxes(&child);
     }
 }
