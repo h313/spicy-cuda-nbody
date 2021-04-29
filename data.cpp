d0b5025ac1ab21d7e6586798fe9c2316d9048ca1
JJ <jjjmase123@gmail.com>
2021-04-28 19:23:18 -0700

    Testing

--- data.cpp
+++ data.cpp
@@ -16,7 +16,7 @@ void DataOutput::write_bounding_box(BoundingBox *bb) {
     boost::qvm::vec<float, 3> min = bb->get_min();
     boost::qvm::vec<float, 3> max = bb->get_max();
     output_stream << "BOUNDINGBOX " << A<0>(min) << " " << A<1>(min) << " " << A<2>(min) << " "
-        A<0>(max) << " " << A<1>(max) << " " << A<2>(max) << std::endl:
+        A<0>(max) << " " << A<1>(max) << " " << A<2>(max) << std::endl;
 }
 
 void DataOutput::write_datapoint(boost::qvm::vec<float, 3> pos) {
@@ -31,11 +31,11 @@ void DataOutput::add_datapoints(Particles &particles) {
 
 void DataOutput::add_octree_bounding_boxes(OctreeNode &parent) {
     // Write the current bounding box
-    write_bounding_box(&(parent->get_bounding_box()));
+    write_bounding_box(&(parent.get_bounding_box()));
 
     // Recurse down through children
     for(int i = 0; i < 8; i++) {
-        OctreeNode *child = parent->get_child(i);
+        OctreeNode *child = parent.get_child(i);
         if(child->get_particle_count() > 1)
             add_octree_bounding_boxes(child);
     }
